823fd9f67e9f0bf3375ab5ad0a0c11503d6cf135
Rubén Ponce <rubenpondebusiness@gmail.com>
2024-10-25 14:21:35 +0200

    Clase dia 24: 404 - Create Error - Implementar manejo de errores y rutas adicionales: - Instalada la dependencia http-errors para crear errores HTTP. - Añadida importación de createError desde http-errors. - Implementadas dos rutas GET para '/user':   > Primera ruta registra la petición y pasa al siguiente middleware.   > Segunda ruta envía una respuesta al cliente. - Añadido middleware para manejar rutas no encontradas (404):   > Utiliza createError para generar un error 404. - Actualizado el manejador de errores para establecer el código de estado HTTP. - Modificado package.json para incluir la nueva dependencia http-errors. - Actualizado package-lock.json con los detalles de la nueva dependencia.

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,6 +2,8 @@
 
 import express from 'express'; // Importa el framework Express para crear aplicaciones web 
 
+import createError from 'http-errors' // Importa el módulo http-errors para crear errores HTTP
+
 const app = express(); // Crea una instancia de la aplicación Express
 
 
@@ -16,6 +18,30 @@ app.get('/', (req, res, next) => {
     // Esta línea no se alcanzará debido al error lanzado.
 }); 
 
+//----------------------------------------------------------------------------
+app.get('/user', (req, res, next) => {
+    console.log('petición a /user')
+    next()
+    })
+
+/**
+   * Define una segunda ruta GET para '/user'.
+   * Esta ruta envía una respuesta al cliente.
+   */
+    app.get('/user', (req, res, next) => {
+    res.send('2222')
+    })
+  
+    /**
+   * Middleware para manejar rutas no encontradas (404).
+   * Crea un error 404 y lo pasa al manejador de errores.
+   */
+    app.use((req, res, next) => {
+    next(createError(404))
+    })
+  
+
+
 // error handler:  Se conoce como "Error Handling Middleware".
 /**En Express.js: un Middleware es una función que tiene acceso al objeto de solicitud (req), al objeto de respuesta (res), y a la siguiente función de middleware en el ciclo de solicitud-respuesta de la aplicación (next).
 El manejador de errores es un Middleware especial que toma cuatro argumentos en lugar de tres: (err, req, res, next). */
@@ -26,9 +52,18 @@ El manejador de errores es un Middleware especial que toma cuatro argumentos en 
  * Express reconoce que es un manejador de errores por la presencia de estos cuatro argumentos.
  * Se utiliza para capturar y procesar errores que ocurren durante la ejecución de la aplicación.
  */
-app.use((err, req, res, next) => {
-    res.send('Ocurrió un error: ' + err.message) // Envía un mensaje de error al cliente.
+/* app.use((err, req, res, next) => {
+    res.send('Ocurrió un error: ' + err.message) 
+    }) // Envía un mensaje de error al cliente. 
+    // se borra para añadir otras...*/
+    
+    //Procesa los errores capturados y envía una respuesta al cliente.
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500)
+        res.send('Ocurrió un error: ' + err.message)
     })
+    
+
 
 
 /* Exporta la aplicación Express configurada */
