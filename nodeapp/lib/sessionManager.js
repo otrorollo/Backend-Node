6b646c3b4eddbd178a95005c2ef22bfcd6b50113
RdRubén <rubenponcebusiness@gmail.com>
2024-12-04 20:52:01 +0100

    Implementar almacenamiento/guardado de sesiones en MongoDB:
    > - Importar MongoStore de connect-mongo en sessionManager.js
    > - Configurar MongoStore para almacenar sesiones en MongoDB
    > - Añadir connect-mongo como dependencia en package.json
    > - Instalar connect-mongo mediante: npm i connect-mongo

--- nodeapp/lib/sessionManager.js
+++ nodeapp/lib/sessionManager.js
@@ -1,4 +1,5 @@
 import session from 'express-session'
+import MongoStore from 'connect-mongo'
 
 const INACTIVITY_EXPIRATION_2_DAYS = 1000 * 60 * 60 * 24 * 2
 /**
@@ -10,7 +11,11 @@ export const middleware = session({
   secret: 'kasdu7ads76sd76ds76ds765ds765dsf',
   saveUninitialized: true,
   resave: false,
-  cookie: { maxAge: INACTIVITY_EXPIRATION_2_DAYS }
+  // configuracion del almacenamiento de las sesiones q se guardan en MongoDB
+  cookie: { maxAge: INACTIVITY_EXPIRATION_2_DAYS },
+  store: MongoStore.create({
+    mongoUrl: 'mongodb://127.0.0.1:27017/cursonode'
+  })
 })
 
 /**
